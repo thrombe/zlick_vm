a9d7bcafce98c1580fc04d37284d92b62b8d31e9
thrombe <yash.n.thombare620@gmail.com>
2024-03-19 01:38:55 +0530

    flake stuff + workspace update

--- flake.nix
+++ flake.nix
@@ -2,17 +2,30 @@
   description = "yaaaaaaaaaaaaaaaaaaaaa";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
-    unstable-nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, unstable-nixpkgs }:
-  let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-    unstable = unstable-nixpkgs.legacyPackages.${system};
-  in
-  {
-    devShells."${system}".default = (import ./shell.nix { inherit pkgs; inherit unstable; });
-  };
+  outputs = inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (system: let
+      overlay-unstable = final: prev: {
+        unstable = import inputs.nixpkgs-unstable {
+          inherit system;
+        };
+      };
+      pkgs = import inputs.nixpkgs {
+        inherit system;
+        overlays = [overlay-unstable];
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          zig_0_10
+          zls
+        ];
+        shellHook = ''
+        '';
+      };
+    });
 }
--- shell.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs ? import <nixpkgs> {}, unstable ? import <nixos-unstable> {} }:
-
-pkgs.mkShell {
-  nativeBuildInputs = with pkgs; [
-    zig_0_10
-    zls
-  ];
-}
